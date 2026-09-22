f4c3cf11106db7a9da14bc4ea9d9f2a4a87af23a
ot6-shibainu <rd0md2rk@sf.commufa.jp>
2026-09-23 07:20:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-const CACHE='ot6-shibainu-ver1.0-final';
-const ASSETS=['./','./index.html','./questions.json','./manifest.webmanifest','./icon-192.png','./icon-512.png','./img/shibasaburo.png','./img/genki.png','./img/momo.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const CACHE='ot6-shibainu-v2.0-250q';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./img/shibasaburo.png','./img/genki.png','./img/momo.png','./questions.json']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
